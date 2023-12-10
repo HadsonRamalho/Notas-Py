a57f9acfc6f923dcd810681a6c75e444180c1993
Hadson <hadsonramalho@gmail.com>
2023-12-10 10:39:17 -0300

    Adicionando função "Salvar como"

--- notas.py
+++ notas.py
@@ -1,35 +1,70 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
-def salvar_arquivo():
-    arquivo = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Arquivos de Texto", "*.txt"), ("Todos os Arquivos", "*.*")])
-    with open(arquivo, 'w') as f:
-        texto = texto_box.get("1.0", "end-1c")
-        f.write(texto)
+class BlocoDeNotas:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Bloco de Notas")
+        self.criar_interface()
 
-def abrir_arquivo():
-    arquivo = filedialog.askopenfilename(filetypes=[("Arquivos de Texto", "*.txt"), ("Todos os Arquivos", "*.*")])
-    with open(arquivo, 'r') as f:
-        conteudo = f.read()
-        texto_box.delete("1.0", "end")
-        texto_box.insert("1.0", conteudo)
+    def criar_interface(self):
+        self.texto_box = tk.Text(self.root, wrap="word", undo=True, width=80, height=20)
+        self.texto_box.pack(expand=True, fill="both")
 
-# interface gráfica
-root = tk.Tk()
-root.title("Bloco de Notas")
+        self.barra_menu = tk.Menu(self.root)
+        self.root.config(menu=self.barra_menu)
 
-texto_box = tk.Text(root, wrap="word", undo=True, width=80, height=20)
-texto_box.pack(expand=True, fill="both")
+        self.criar_menu_arquivo()
 
-barra_menu = tk.Menu(root)
-root.config(menu=barra_menu)
+    def criar_menu_arquivo(self):
+        menu_arquivo = tk.Menu(self.barra_menu, tearoff=0)
+        self.barra_menu.add_cascade(label="Arquivo", menu=menu_arquivo)
+        menu_arquivo.add_command(label="Novo", command=self.novo_arquivo)
+        menu_arquivo.add_command(label="Abrir", command=self.abrir_arquivo)
+        menu_arquivo.add_command(label="Salvar", command=self.salvar_arquivo)
+        menu_arquivo.add_command(label="Salvar Como", command=self.salvar_como_arquivo)
+        menu_arquivo.add_separator()
+        menu_arquivo.add_command(label="Sair", command=self.sair)
 
-menu_arquivo = tk.Menu(barra_menu, tearoff=0)
-barra_menu.add_cascade(label="Arquivo", menu=menu_arquivo)
-menu_arquivo.add_command(label="Novo")
-menu_arquivo.add_command(label="Abrir", command=abrir_arquivo)
-menu_arquivo.add_command(label="Salvar", command=salvar_arquivo)
-menu_arquivo.add_separator()
-menu_arquivo.add_command(label="Sair", command=root.destroy)
+    def novo_arquivo(self):
+        self.texto_box.delete("1.0", "end")
 
-root.mainloop()
+    def abrir_arquivo(self):
+        arquivo = filedialog.askopenfilename(filetypes=[("Arquivos de Texto", "*.txt"), ("Todos os Arquivos", "*.*")])
+        if arquivo:
+            try:
+                with open(arquivo, 'r') as f:
+                    conteudo = f.read()
+                    self.texto_box.delete("1.0", "end")
+                    self.texto_box.insert("1.0", conteudo)
+            except Exception as e:
+                messagebox.showerror("Erro ao abrir arquivo", str(e))
+
+    def salvar_arquivo(self):
+        arquivo = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Arquivos de Texto", "*.txt"), ("Todos os Arquivos", "*.*")])
+        if arquivo:
+            try:
+                with open(arquivo, 'w') as f:
+                    texto = self.texto_box.get("1.0", "end-1c")
+                    f.write(texto)
+            except Exception as e:
+                messagebox.showerror("Erro ao salvar arquivo", str(e))
+
+    def salvar_como_arquivo(self):
+        arquivo = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Arquivos de Texto", "*.txt"), ("Todos os Arquivos", "*.*")])
+        if arquivo:
+            try:
+                with open(arquivo, 'w') as f:
+                    texto = self.texto_box.get("1.0", "end-1c")
+                    f.write(texto)
+            except Exception as e:
+                messagebox.showerror("Erro ao salvar arquivo", str(e))
+
+    def sair(self):
+        self.root.destroy()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = BlocoDeNotas(root)
+    root.mainloop()
